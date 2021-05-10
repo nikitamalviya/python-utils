c33f87350cca7ee5d30e0d808483618432e91227
Nikita Malviya <ernikitamalviya09@gmail.com>
2021-05-10 22:44:04 +0530

    Update break_string_into_n_parts.py

--- break_string_into_n_parts.py
+++ break_string_into_n_parts.py
@@ -4,7 +4,8 @@ mystring = '123456789abcdefghijkl'
 
 print(textwrap.wrap(mystring, 4))
 
-n = 4
+len_of_substring = 4
+
 # using list comprehension 
-sentences = [mystring[i * n:(i + 1) * n] for i in range((len(mystring) + n - 1) // n )] 
+sentences = [mystring[i * len_of_substring:(i + 1) * len_of_substring] for i in range((len(mystring) + len_of_substring - 1) // len_of_substring )] 
 print("sentence 1 : ", sentences) 
