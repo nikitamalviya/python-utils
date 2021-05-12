8175bfdab24b0bf121f965a63c36302dbae4fe62
Nikita Malviya <ernikitamalviya09@gmail.com>
2021-05-12 03:55:24 -0700

    Update install__pip_conda_env.py

--- install__pip_conda_env.py
+++ install__pip_conda_env.py
@@ -11,19 +11,29 @@ python -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.
 python3 -m venv ~/py36
 # or
 python -m venv c:\path\to\py36
-  
 # activate
 source ~/py36/bin/activate
+# list installed packages
+pip freeze 
+
 
 
 # for conda trusted host
 conda config --set ssl_verify no
 # conda create new env
-conda create -n uw36 python=3.6
+conda create -n my_env python=3.6
+# install using requirements.txt
+conda install -n <env_name> requirements.txt
+# save requirements.txt 
+conda list -e > requirements.txt
+# install particular packages while creating env
+conda create -n <env_name> python=<version#> <packagename> <packagename> <packagename>=<version#>
 # install package using conda
 conda install -c conda-forge opencv=3.2.0
 # create conda env at a location
 conda create --prefix E:\Anaconda\env_name python=3.8
+# list packages
+conda list
 # update conda
 conda update -n base -c defaults conda
 # install requirements.txt using conda 
