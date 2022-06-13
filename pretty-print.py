65c703dd6b291e6c913b31b06e6be1d0f3662afd
Nikita Malviya <ernikitamalviya09@gmail.com>
2022-06-13 18:23:21 +0530

    Create pretty-print.py

---
+++ pretty-print.py
@@ -0,0 +1,12 @@
+# ! pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -U requests, pprint, rich
+
+import requests
+from pprint import pprint
+from rich import print_json
+
+with open(image_path, 'rb') as f:
+    multipart_form_data = {'file':  f}
+    response = requests.post(url=url, files=multipart_form_data, verify=False) # data= {"source":"test"}, 
+    response = response.json()
+    pretty_response = json.dumps(response, indent=4)
+    print_json(pretty_response)
