a50071c51617b28524dd97f1abc5a50e416d0b65
Nikita Malviya <ernikitamalviya09@gmail.com>
2022-06-15 22:21:24 +0530

    set it trusted host

--- install__pip_conda_env.py
+++ install__pip_conda_env.py
@@ -1,3 +1,6 @@
+# set it once
+pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org" --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org
+
 # upgrade pip or any package
 python -m pip install --upgrade pip
 # install package
