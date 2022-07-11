952e16bf77a2a0bfa9864a821f0135bfeed53a9e
Nikita Malviya <ernikitamalviya09@gmail.com>
2022-07-11 14:27:34 +0530

    display file name too.

--- try_except.py
+++ try_except.py
@@ -3,5 +3,5 @@ import sys
 try:
     a = 10
     a = a + 'hey'
-except Exception as e:
-    print(f"Exception Raised : {e}, errorOnLine: {sys.exc_info()[-1].tb_lineno}")
+except Exception as e :
+    click.secho(f"Exception Raised : {e}, errorOnLine: {sys.exc_info()[-1].tb_lineno}, file : {os.path.basename(__file__)}", fg="red")
