ab3e08872b1566e94cd61ec80c24b23c3e05c128
Nikita Malviya <ernikitamalviya09@gmail.com>
2024-02-27 16:34:20 +0530

    Update and rename python_colors.txt to python_colors.py

---
+++ python_colors.py
@@ -0,0 +1,36 @@
+
+color_codes = [
+    '#FF0000',  # Red
+    '#00FF00',  # Green
+    '#0000FF',  # Blue
+    '#FFFF00',  # Yellow
+    '#00FFFF',  # Cyan
+    '#FF00FF',  # Magenta
+    '#C0C0C0',  # Silver
+    '#808080',  # Gray
+    '#000000',  # Black
+    '#FFFFFF',  # White
+    '#F0F8FF',  # AliceBlue
+    '#FAEBD7',  # AntiqueWhite
+    '#00FFFF',  # Aqua
+    '#F0FFFF',  # Azure
+    '#D2B48C',  # Tan
+]
+
+color_codes = [
+    (255, 0, 0),  # Red
+    (0, 255, 0),  # Green
+    (0, 0, 255),  # Blue
+    (255, 255, 0),  # Yellow
+    (0, 255, 255),  # Cyan
+    (255, 0, 255),  # Magenta
+    (192, 192, 192),  # Silver
+    (128, 128, 128),  # Gray
+    (0, 0, 0),       # Black
+    (255, 255, 255),  # White
+    (240, 248, 255),  # AliceBlue
+    (250, 235, 215),  # AntiqueWhite
+    (0, 255, 255),  # Aqua (already included)
+    (240, 248, 255),  # Azure (same as AliceBlue)
+    (210, 180, 140)   # Tan
+]
