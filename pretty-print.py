a6cb07411b4a01daa07d62de6995742a85f077eb
Nikita Malviya <ernikitamalviya09@gmail.com>
2024-05-20 14:19:15 +0530

    Update pretty-print.py

--- pretty-print.py
+++ pretty-print.py
@@ -4,6 +4,13 @@ import requests
 from pprint import pprint
 from rich import print_json
 
+
+# sort the dictionary key's values and pretty print the text    
+classes_count = {}
+classes_count = sorted(classes_count.items(), key=lambda item: item[1], reverse=True)
+click.secho(f"Sorted dictionary : ", fg="green")
+pprint(classes_count)
+
 with open(image_path, 'rb') as f:
     multipart_form_data = {'file':  f}
     response = requests.post(url=url, files=multipart_form_data, verify=False) # data= {"source":"test"}, 
