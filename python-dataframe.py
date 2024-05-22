86930173c25d728d392b440f259409d1c6ed842a
Nikita Malviya <ernikitamalviya09@gmail.com>
2024-05-22 10:52:58 +0530

    Update python-dataframe.py

--- python-dataframe.py
+++ python-dataframe.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
+### Add a row at an index, index starts from 0 in dataframe
 df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-# Add a row at index 1 with values 10 and 20 in columns A and B respectively
-df.loc[1] = [10, 20]
+df.loc[index] = [10, 20]
 print(df)
